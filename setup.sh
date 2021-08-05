cc9249e3597d99dff064c94a1bb6cf40fceeac20
anastasia <ak@braingarden.ai>
2021-08-05 06:14:44 +0300

    init

--- setup.sh
+++ setup.sh
@@ -1,21 +1,19 @@
-FROM ubuntu:latest
+#!/bin/bash
 
-ENV DEBIAN_FRONTEND=noninteractive
-PHANTOMJS_LINK=""
+export LINK="https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-2.1.1-linux-x86_64.tar.bz2"
 
-RUN apt-get update \
+apt-get update \
   && apt-get install -y python3-pip python3-dev wget \
   && cd /usr/local/bin \
   && ln -s /usr/bin/python3 python \
   && pip3 --no-cache-dir install --upgrade pip \
+  && pip3 install virtualenv \
   && rm -rf /var/lib/apt/lists/*
 
-RUN wget -O phantomjs.tar.bz2 $PHANTOMJS_LINK &&\
+wget -O phantomjs.tar.bz2 $PHANTOMJS_LINK &&\
     mkdir phantomjs && \
     tar -xvf phantomjs.tar.bz2 --directory phantomjs &&  \
     export PATH=${PATH}:/phantomjs/bin/phantomjs && \
     alias phantomjs="$/phantomjs/bin/phantomjs"
 
-RUN apt clean  && \
-    && apt update && \
-    apt install -y git-all
+apt-get update && apt-get install -y git-all
